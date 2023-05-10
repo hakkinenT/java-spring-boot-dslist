3b8966f647b20a551ddccb61bc72c5641efc02ae
Tawanna Hakkinen <thol_16@hotmail.com>
2023-05-10 12:49:02 -0300

    consulta sql, projection, buscar games por lista

--- src/main/java/com/hakkinen/dslist/controllers/GameListController.java
+++ src/main/java/com/hakkinen/dslist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package com.hakkinen.dslist.controllers;
 
 import com.hakkinen.dslist.dto.GameListDTO;
+import com.hakkinen.dslist.dto.GameMinDTO;
 import com.hakkinen.dslist.services.GameListService;
+import com.hakkinen.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,18 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
         return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        List<GameMinDTO> games = gameService.findByList(listId);
+        return games;
     }
 }
--- src/main/java/com/hakkinen/dslist/services/GameService.java
+++ src/main/java/com/hakkinen/dslist/services/GameService.java
@@ -3,10 +3,12 @@ package com.hakkinen.dslist.services;
 import com.hakkinen.dslist.dto.GameDTO;
 import com.hakkinen.dslist.dto.GameMinDTO;
 import com.hakkinen.dslist.entities.Game;
+import com.hakkinen.dslist.projections.GameMinProjection;
 import com.hakkinen.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
@@ -25,6 +27,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> games = result.stream().map(g -> new GameMinDTO(g)).toList();
+        return games;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> games = result.stream().map(g -> new GameMinDTO(g)).toList();
         return games;
     }
